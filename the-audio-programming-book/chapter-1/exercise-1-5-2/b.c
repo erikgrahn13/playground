aab4af07bce34bed2bfd142d6605b1c6a3198c5d
Erik Grahn <erikgrahn13@gmail.com>
2025-02-14 14:03:33 +0100

    complete exercise 1.5.2 in the audio programmer book

---
+++ the-audio-programming-book/chapter-1/exercise-1-5-2/b.c
@@ -0,0 +1,66 @@
+/* nscale.c: Display E.T frequencies for an N-note octave, from a
+given MIDI note */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(int argc, char *argv[])
+{
+    int notes, i, midinote;
+    double frequency, ratio;
+    double c0, c5;
+    double intervals[24];
+    if (argc != 3)
+    {
+        printf("Usage: nscale notes miditnote\n");
+        printf("This program outputs the frequency in Hz based on the number of notes that it's contained in one octave (temperament).");
+        return 1;
+    }
+    notes = atoi(argv[1]);
+    if (notes < 1)
+    {
+        printf("Error: notes must be positive\n");
+        return 1;
+    }
+    if (notes > 24)
+    {
+        printf("Error: maximum value for notes is 24\n");
+        return 1;
+    }
+    midinote = atoi(argv[2]);
+    if (midinote < 0)
+    {
+        printf("Error: cannot have negative MIDI notes!\n");
+        return 1;
+    }
+    if(midinote > 127)
+    {
+        printf("Error: maximum midinote is 127\n");
+        return 1;
+    }
+    /*** find the frequency of the MIDI note ***/
+    /* calc standard E.T semitone ratio */
+    ratio = pow(2.0, 1.0 / 12.0);
+    /* find Middle C, three semitones above low A = 220 */
+    c5 = 220.0 * pow(ratio, 3);
+    /* MIDI note 0 is C, 5 octaves below Middle C */
+    c0 = c5 * pow(0.5, 5);
+    frequency = c0 * pow(ratio, midinote);
+    /* calc ratio from notes, and fill the frequency array */
+    ratio = pow(2.0, 1.0 / notes);
+    
+    // (b). When doing a do while here Im relying that the there exists at least one intervals stored.
+    do
+    {
+        intervals[i] = frequency;
+        frequency *= ratio;
+        i++;
+    } while(i < notes);
+    /* finally, read array, write to screen */
+    i = 0;
+    do
+    {
+        printf("%f\n", intervals[i]);
+        i++;
+    } while(i < notes);
+    return 0;
+}
